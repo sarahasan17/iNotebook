d7b7210fdb8b1b668afda050d8859812ad79cb68
Sara Hasan <103211125+sarahasan17@users.noreply.github.com>
2024-09-24 21:01:07 +0530

    jwt and bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const User = require("../models/User");
+var bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
 const router = express.Router();
-
+var jwt = require("jsonwebtoken");
+const SECRET_TOKEN='saraisagoodgirl';
 router.post(
   "/createUser",
   [
@@ -11,6 +13,7 @@ router.post(
     body("password", "Enter a valid password").isLength({ min: 8 }),
   ],
   async (req, res) => {
+    //res.json({amma:"amma"})
     const result = validationResult(req);
     if (!result.isEmpty()) {
       return res.status(400).json({ result: result.array() });
@@ -20,20 +23,20 @@ router.post(
        return res.status(400).json({ error: "Email already exists" });
      }
     try{
+      const salt=await bcrypt.genSalt(10);
+      const secPass=await bcrypt.hash(req.body.password,salt)
       user=await User.create({
       name: req.body.name,
-      password: req.body.password,
+      password: secPass,
       email: req.body.email,
     })
-      // .then((user) => res.json(user))
-      // .catch((err) => {
-      //   console.log(err);
-      //   res.json({
-      //     error: "please enter a unique value",
-      //     message: err.message,
-      //   });
-      // });
-      res.json(user)
+    const data={
+      user:{
+        id:user.id
+      }
+    }
+    const authtoken=jwt.sign(data,SECRET_TOKEN)
+    res.json({authtoken})
     }
     catch(error){
       console.error(error.message)
