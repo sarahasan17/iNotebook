864b5fd8996508def6dfcc004d7f6e2951805739
Sara Hasan <103211125+sarahasan17@users.noreply.github.com>
2024-09-25 16:19:05 +0530

    try and catch update

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -17,20 +17,25 @@ router.post(
     body("description", "Enter a valid description").exists(),
   ],
   async (req, res) => {
-    const { title, description, tag } = req.body;
-    const result = validationResult(req);
-    if (!result.isEmpty()) {
-      return res.status(400).json({ result: result.array() });
-    }
-    const notes = new Notes({
-      title,
-      description,
-      tag,
-      user: req.user.id,
-    });
-    const savednote = await notes.save();
-    res.json(savednote);
-  }
+   try {
+     const { title, description, tag } = req.body;
+     const result = validationResult(req);
+     if (!result.isEmpty()) {
+       return res.status(400).json({ result: result.array() });
+     }
+     const notes = new Notes({
+       title,
+       description,
+       tag,
+       user: req.user.id,
+     });
+     const savednote = await notes.save();
+     res.json(savednote);
+   } catch (error) {
+     console.error(error.message);
+     res.status(500).json("Bad request found");
+   }
+   }
 );
 
 //update notes
@@ -41,31 +46,37 @@ router.put(
     body("title", "Enter a valid title").isLength({ min: 5 }),
     body("description", "Enter a valid description").exists(),
   ],
-  async (req, res) => {
-    const { title, description, tag } = req.body;
-    const newNote = {};
-    if (title) {
-      newNote.title = title;
-    }
-    if (description) {
-      newNote.description = description;
-    }
-    if (tag) {
-      newNote.tag = tag;
-    }
-    let note = await Notes.findById(req.params.id);
-    if (!note) {
-      res.status(404).send("Not allowed");
-    }
-    if (note.user.toString() !== req.user.id) {
-      res.status(401).send("Not valid match found");
-    }
-    note = await Notes.findByIdAndUpdate(
-      req.params.id,
-      { $set: newNote },
-      { new: true }
-    );
-    res.json({ note });
+  
+    async (req, res) => {
+      try {
+        const { title, description, tag } = req.body;
+        const newNote = {};
+        if (title) {
+          newNote.title = title;
+        }
+        if (description) {
+          newNote.description = description;
+        }
+        if (tag) {
+          newNote.tag = tag;
+        }
+        let note = await Notes.findById(req.params.id);
+        if (!note) {
+          res.status(404).send("Not allowed");
+        }
+        if (note.user.toString() !== req.user.id) {
+          res.status(401).send("Not valid match found");
+        }
+        note = await Notes.findByIdAndUpdate(
+          req.params.id,
+          { $set: newNote },
+          { new: true }
+        );
+        res.json({ note });
+      } catch (error) {
+        console.error(error.message);
+        res.status(500).json("Bad request found");
+      }
   }
 );
 
@@ -74,17 +85,20 @@ router.delete(
   "/deleteNotes/:id",
   fetchUser,
   async (req, res) => {
-    let note = await Notes.findById(req.params.id);
-    if (!note) {
-      res.status(404).send("Not allowed");
+    try {
+      let note = await Notes.findById(req.params.id);
+      if (!note) {
+        res.status(404).send("Not allowed");
+      }
+      if (note.user.toString() !== req.user.id) {
+        res.status(401).send("Not valid match found");
+      }
+      note = await Notes.findByIdAndDelete(req.params.id);
+      res.json({ Success: "deletion complete" });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json("Bad request found");
     }
-    if (note.user.toString() !== req.user.id) {
-      res.status(401).send("Not valid match found");
-    }
-    note = await Notes.findByIdAndDelete(
-      req.params.id
-    );
-    res.json({"Success":"deletion complete"});
   }
 );
 module.exports = router;
